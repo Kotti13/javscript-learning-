baa3b7926fa1d3b7fc47d635b7b7713efc54236d
Kotti13 <skottai493@gmail.com>
2024-10-05 11:07:09 +0530

    new methods

---
+++ jsnewmethodsfrom(gokul).js
@@ -0,0 +1,8 @@
+// let arr=['A','C','E'];
+// console.log(String.fromCharCode(arr[arr.length-1].charCodeAt()+2))
+// let str= "Kottai Pranesh Gokul";
+// console.log(str.split("").filter(x=>x>="A"&&x<="Z").join(""));
+
+let array=[1,2,3,4,5];
+let array2=array.map(x=>x*x);
+console.log(array2);
