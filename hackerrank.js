4bb9a93ac749bbd85fcaadc32a54dbc6748cc6b3
Kotti13 <skottai493@gmail.com>
2024-10-05 11:08:36 +0530

    hackerrank problems

---
+++ hackerrank.js
@@ -0,0 +1,13 @@
+// let arr=[[1,2,3],[4,5,6],[9,8,9]]
+// let sum1=0;
+// let sum2=0;
+// for(let i=0;i<arr.length;i++){
+//   sum1=sum1+arr[i][i];
+//   sum2=sum2+arr[i][arr.length-1-i];
+// }
+// if(sum1>sum2){
+//   console.log(sum1-sum2);
+// }
+// else{
+//  console.log (sum2-sum1);
+// }
